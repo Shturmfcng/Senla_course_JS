fbc6858f0270f471e06f4aa6e88282a36611f55d
Mikhail _Gotsko <Shturmfcng@gmail.com>
2021-04-23 21:30:28 +0300

    feat: Added cross-browser compatibility except IE.

--- webpack.config.js
+++ webpack.config.js
@@ -2,55 +2,94 @@
  * Webpack main configuration file
  */
 
-const path = require('path');
-const fs = require('fs');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const { extendDefaultPlugins } = require('svgo');
+const path = require("path");
+const fs = require("fs");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const { extendDefaultPlugins } = require("svgo");
+const autoprefixer = require("autoprefixer");
+const environment = require("./configuration/environment");
 
-const environment = require('./configuration/environment');
+const templateFiles = fs
+  .readdirSync(environment.paths.source)
+  .filter((file) => path.extname(file).toLowerCase() === ".html");
 
-const templateFiles = fs.readdirSync(environment.paths.source)
-  .filter((file) => path.extname(file).toLowerCase() === '.html');
-
-const htmlPluginEntries = templateFiles.map((template) => new HTMLWebpackPlugin({
-  inject: true,
-  hash: false,
-  filename: template,
-  template: path.resolve(environment.paths.source, template),
-  favicon: path.resolve(environment.paths.source, 'images', 'favicon.ico'),
-}));
+const htmlPluginEntries = templateFiles.map(
+  (template) =>
+    new HTMLWebpackPlugin({
+      inject: true,
+      hash: false,
+      filename: template,
+      template: path.resolve(environment.paths.source, template),
+      favicon: path.resolve(environment.paths.source, "images", "favicon.ico"),
+    })
+);
 
 module.exports = {
   entry: {
-    app: path.resolve(environment.paths.source, 'js', 'app.js'),
+    app: path.resolve(environment.paths.source, "js", "app.js"),
   },
   output: {
-    filename: 'js/[name].js',
+    filename: "js/[name].js",
     path: environment.paths.output,
   },
   module: {
     rules: [
       {
-        test: /\.((c|sa|sc)ss)$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+        test: /\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  autoprefixer({
+                    overridebrowsers: ["ie >= 8", "last 4 version"],
+                  }),
+                ],
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  autoprefixer({
+                    overridebrowsers: ["ie >= 8", "last 4 version"],
+                  }),
+                ],
+              },
+            },
+          },
+          "sass-loader",
+        ],
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ['babel-loader'],
+        use: ["babel-loader"],
       },
       {
         test: /\.(png|gif|jpe?g|svg)$/i,
         use: [
           {
-            loader: 'url-loader',
+            loader: "url-loader",
             options: {
-              name: 'images/design/[name].[hash:6].[ext]',
-              publicPath: '../',
+              name: "images/design/[name].[hash:6].[ext]",
+              publicPath: "../",
               limit: environment.limits.images,
             },
           },
@@ -60,10 +99,10 @@ module.exports = {
         test: /\.(eot|ttf|woff|woff2)$/,
         use: [
           {
-            loader: 'url-loader',
+            loader: "url-loader",
             options: {
-              name: 'fonts/[name].[hash:6].[ext]',
-              publicPath: '../',
+              name: "fonts/[name].[hash:6].[ext]",
+              publicPath: "../",
               limit: environment.limits.fonts,
             },
           },
@@ -73,7 +112,7 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: 'css/[name].css',
+      filename: "css/[name].css",
     }),
     new ImageMinimizerPlugin({
       test: /\.(jpe?g|png|gif|svg)$/i,
@@ -81,15 +120,15 @@ module.exports = {
         // Lossless optimization with custom option
         // Feel free to experiment with options for better result for you
         plugins: [
-          ['gifsicle', { interlaced: true }],
-          ['jpegtran', { progressive: true }],
-          ['optipng', { optimizationLevel: 5 }],
+          ["gifsicle", { interlaced: true }],
+          ["jpegtran", { progressive: true }],
+          ["optipng", { optimizationLevel: 5 }],
           [
-            'svgo',
+            "svgo",
             {
               plugins: extendDefaultPlugins([
                 {
-                  name: 'removeViewBox',
+                  name: "removeViewBox",
                   active: false,
                 },
               ]),
@@ -100,20 +139,20 @@ module.exports = {
     }),
     new CleanWebpackPlugin({
       verbose: true,
-      cleanOnceBeforeBuildPatterns: ['**/*', '!stats.json'],
+      cleanOnceBeforeBuildPatterns: ["**/*", "!stats.json"],
     }),
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: path.resolve(environment.paths.source, 'images', 'content'),
-          to: path.resolve(environment.paths.output, 'images', 'content'),
-          toType: 'dir',
+          from: path.resolve(environment.paths.source, "images", "content"),
+          to: path.resolve(environment.paths.output, "images", "content"),
+          toType: "dir",
           globOptions: {
-            ignore: ['*.DS_Store', 'Thumbs.db'],
+            ignore: ["*.DS_Store", "Thumbs.db"],
           },
         },
       ],
     }),
   ].concat(htmlPluginEntries),
-  target: 'web',
+  target: "web",
 };
